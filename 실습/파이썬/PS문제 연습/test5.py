92501d1f51e726c7703d4161790b297af670ce6d
oh_jun <vhzkclq0705@naver.com>
2025-01-03 12:30:49 +0900

    [Update] 입력수정

--- 실습/파이썬/PS문제 연습/test5.py
+++ 실습/파이썬/PS문제 연습/test5.py
@@ -2,9 +2,9 @@ def calculate(n):
     return n // 7 + (1 if n % 7 else 0)
 
 n = int(input("피자를 나눠먹을 사람의 수: "))
-if n < 1 or n > 100:
-    print("잘못된 입력입니다. 프로그램을 종료합니다.")
-    exit()
+while n < 1 or n > 100:
+    print("1과 100 사이의 정수를 입력해주세요.")
+    n = int(input("피자를 나눠먹을 사람의 수: "))
 
 result = calculate(n)
 print(f'7명이 최소 한 조각씩 먹기 위해서 최소 {result}판이 필요합니다.')
