4606526dc7214e624341b7be18874c716c9dbf94
oh_jun <vhzkclq0705@naver.com>
2025-01-03 12:34:41 +0900

    [Update] 입력수정

--- 실습/파이썬/PS문제 연습/test4.py
+++ 실습/파이썬/PS문제 연습/test4.py
@@ -1,15 +1,15 @@
-def fail_validation():
-    print("잘못된 정수를 입력하였습니다. 프로그램을 종료합니다.")
-    exit()
-
 def sqrt(n, t):
     return n ** t
 
 n = int(input("처음 세균의 마리수를 입력하세요: "))
-if n < 1 or n > 10: fail_validation()
+while not (1 <= n <= 10):
+    print("1과 10 사이의 정수를 입력해주세요.")
+    n = int(input("처음 세균의 마리수를 입력하세요: "))
+    
 t = int(input("경과한 시간을 입력하세요: "))
-if t < 1 or t > 15: fail_validation()
+while not (1 <= t <= 15):
+    print("1과 15 사이의 정수를 입력해주세요.")
+    t = int(input("경과한 시간을 입력하세요: "))
 
 result = sqrt(n, t)
-
 print(f'처음엔 {n}마리, {t}시간 후엔 {result}마리가 됩니다. 따라서 {result}를 return합니다.')
